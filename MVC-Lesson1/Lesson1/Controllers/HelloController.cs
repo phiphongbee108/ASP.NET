4f03b0ba39a046a1f1a4459dd7f018276750c942
Tuong Duc Tam <TamTD@vietis.vn>
2022-04-19 09:43:01 +0700

    lesson 1

---
+++ MVC-Lesson1/Lesson1/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        //GET: Hello/SayHello
+        public ActionResult SayHello()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC5";
+            return View();
+        }
+    }
+}
---
+++ MVC-Lesson1/Lesson1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Search()
+        {
+            ViewBag.Message = "Search";
+            return View();
+        }
+
+        // GET: Product
+        public ActionResult HelloWorld()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+
+        // GET: Product
+        public ActionResult Hello()
+        {
+            ViewBag.Message = "Hello";
+            return View();
+        }
+
+        // GET: Product
+        public ActionResult World()
+        {
+            ViewBag.Message = "World";
+            return View();
+        }
+    }
+}
